8d2ed24a2ea7c23aff883ba167e42f2327a9b9b0
Lenilson Soares <lenilsonp40@gmail.com>
2024-08-05 11:24:07 -0300

    init

---
+++ fundamentals/fundamentals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var notaDigitada = Console.ReadLine();
+
+            var nota = int.Parse(notaDigitada);
+
+            if (nota >= 0)
+            {
+
+            }
+
+            // Arrays
+
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var numberCopy = new int[10];
+
+        }
+    }
+}
