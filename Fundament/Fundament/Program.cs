dd370c539889e2ffde1d97539da63a45288cd71e
Lenilson Soares <lenilsonp40@gmail.com>
2024-08-15 09:19:16 -0300

    alterando Pastas

---
+++ Fundament/Fundament/Program.cs
@@ -0,0 +1,28 @@
+﻿//Parei aula F76
+
+using System.Globalization;
+
+namespace Fundament;
+
+class Program
+{
+    static void Main()
+    {
+        DateOnly dia = new DateOnly(2023, 12, 2);
+
+        string diaEmTexto = dia.ToString("dd/MMMM/yyyy", new CultureInfo("pt-BR"));
+
+        DateTime hoje = DateTime.UtcNow;
+
+
+        Console.WriteLine(hoje);
+
+
+
+        for (int i = 3; i > 2; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
