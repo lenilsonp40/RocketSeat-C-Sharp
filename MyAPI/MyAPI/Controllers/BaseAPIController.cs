d1c62bcc4540baa7486e7f4dded2e83cf57a530c
Lenilson Soares <lenilsonp40@gmail.com>
2024-08-21 10:33:32 -0300

    baseController

---
+++ MyAPI/MyAPI/Controllers/BaseAPIController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class BaseAPIController : ControllerBase
+    {
+        [HttpGet("heathy")]
+        public IActionResult Heathy()
+        {
+            return Ok("It´s working");
+        }
+    }
+}
--- MyAPI/MyAPI/Controllers/UserController.cs
+++ MyAPI/MyAPI/Controllers/UserController.cs
@@ -1,20 +1,16 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MyAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
+   
+    public class UserController : BaseAPIController
     {
         [HttpGet]
         [Route("{id}")]
         public IActionResult GetById([FromRoute] int id)
         {
-
-
             return Ok();
         }
-
+        
     }
 }
